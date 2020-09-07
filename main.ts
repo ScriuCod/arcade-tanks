c2f3f64a9b64114d9ba35e3197deb2d46e2c03ff
stefanspt <63734169+stefanspt@users.noreply.github.com>
2020-09-07 10:11:30 +0200

    I animated the tanks wheels

--- main.ts
+++ main.ts
@@ -1,1 +1,59 @@
-
+scene.setBackgroundColor(7)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . e e . . . . . . . 
+    . . . . . . . e e . . . . . . . 
+    . f f f . . . e e . . . f f f . 
+    . f f f f f f e e f f f f d f . 
+    . f d f 4 4 4 e e 4 4 4 f f f . 
+    . f f f 4 4 4 e e 4 4 4 f d f . 
+    . f d f 4 4 2 e e 2 4 4 f f f . 
+    . f f f 4 4 2 e e 2 4 4 f d f . 
+    . f d f 4 4 2 2 2 2 4 4 f f f . 
+    . f f f 4 4 4 4 4 4 4 4 f d f . 
+    . f d f 4 f 4 f f 4 f 4 f f f . 
+    . f f f 4 4 4 4 4 4 4 4 f d f . 
+    . f d f f f f f f f f f f f f . 
+    . f f f . . . . . . . . f f f . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+[img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . e e . . . . . . . 
+    . . . . . . . e e . . . . . . . 
+    . f f f . . . e e . . . f f f . 
+    . f d f f f f e e f f f f d f . 
+    . f f f 4 4 4 e e 4 4 4 f f f . 
+    . f d f 4 4 4 e e 4 4 4 f d f . 
+    . f f f 4 4 2 e e 2 4 4 f f f . 
+    . f d f 4 4 2 e e 2 4 4 f d f . 
+    . f f f 4 4 2 2 2 2 4 4 f f f . 
+    . f d f 4 4 4 4 4 4 4 4 f d f . 
+    . f f f 4 f 4 f f 4 f 4 f f f . 
+    . f d f 4 4 4 4 4 4 4 4 f d f . 
+    . f f f f f f f f f f f f f f . 
+    . f d f . . . . . . . . f d f . 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . e e . . . . . . . 
+    . . . . . . . e e . . . . . . . 
+    . f d f . . . e e . . . f d f . 
+    . f f f f f f e e f f f f f f . 
+    . f d f 4 4 4 e e 4 4 4 f d f . 
+    . f f f 4 4 4 e e 4 4 4 f f f . 
+    . f d f 4 4 2 e e 2 4 4 f d f . 
+    . f f f 4 4 2 e e 2 4 4 f f f . 
+    . f d f 4 4 2 2 2 2 4 4 f d f . 
+    . f f f 4 4 4 4 4 4 4 4 f f f . 
+    . f d f 4 f 4 f f 4 f 4 f d f . 
+    . f f f 4 4 4 4 4 4 4 4 f f f . 
+    . f d f f f f f f f f f f d f . 
+    . f f f . . . . . . . . f f f . 
+    . . . . . . . . . . . . . . . . 
+    `],
+100,
+true
+)
