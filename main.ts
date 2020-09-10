861d5c7e26ba55ceee48b81d46cd1cc2b47133fc
stefanspt <63734169+stefanspt@users.noreply.github.com>
2020-09-10 09:23:25 +0200

    I started map

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.changeScoreBy(1)
+})
+function randomWalls () {
+    tiles.setTilemap(tiles.createTilemap(hex`0a0008000000000000000000000001010101010101010101010201020101020102010101010103030101010101010101030301010101010201020101020102010101010101010101010102020202020202020202`, img`
+        2 2 2 2 2 2 2 2 2 2 
+        . . . . . . . . . . 
+        . . . . . . . . . . 
+        . . . . . . . . . . 
+        . . . . . . . . . . 
+        . . . . . . . . . . 
+        . . . . . . . . . . 
+        2 2 2 2 2 2 2 2 2 2 
+        `, [myTiles.transparency16,sprites.castle.tilePath5,sprites.dungeon.floorLight0,myTiles.tile1], TileScale.Sixteen))
+}
 scene.setBackgroundColor(7)
-let mySprite = sprites.create(img`
+let tankCharacter = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . e e . . . . . . . 
     . . . . . . . e e . . . . . . . 
@@ -18,7 +33,7 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 animation.runImageAnimation(
-mySprite,
+tankCharacter,
 [img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . e e . . . . . . . 
@@ -57,3 +72,17 @@ mySprite,
 100,
 true
 )
+info.setScore(0)
+tankCharacter.setPosition(134, 93)
+controller.moveSprite(tankCharacter)
+tankCharacter.setFlag(SpriteFlag.StayInScreen, true)
+tiles.setTilemap(tiles.createTilemap(hex`0a0008000000000000000000000001010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010102020202020202020202`, img`
+    2 2 2 2 2 2 2 2 2 2 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    2 2 2 2 2 2 2 2 2 2 
+    `, [myTiles.transparency16,sprites.castle.tilePath5,sprites.dungeon.floorLight0], TileScale.Sixteen))
