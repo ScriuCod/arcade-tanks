ce4b484e5b8528ed3d4d3669918e6ffc32208f18
stefanspt <63734169+stefanspt@users.noreply.github.com>
2020-09-16 18:05:28 +0200

    I started tank animations and shooting

--- main.ts
+++ main.ts
@@ -1,3 +1,99 @@
+namespace SpriteKind {
+    export const Wall = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    setOrientation("up")
+})
+function setOrientation (newOrientation: string) {
+    if (newOrientation == "up") {
+        sprites.setDataString(tankCharacter, "orientation", newOrientation)
+        tankCharacter.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . e e . . . . . . . 
+            . . . . . . . e e . . . . . . . 
+            . f f f . . . e e . . . f f f . 
+            . f f f f f f e e f f f f d f . 
+            . f d f 4 4 4 e e 4 4 4 f f f . 
+            . f f f 4 4 4 e e 4 4 4 f d f . 
+            . f d f 4 4 2 e e 2 4 4 f f f . 
+            . f f f 4 4 2 e e 2 4 4 f d f . 
+            . f d f 4 4 2 2 2 2 4 4 f f f . 
+            . f f f 4 4 4 4 4 4 4 4 f d f . 
+            . f d f 4 f 4 f f 4 f 4 f f f . 
+            . f f f 4 4 4 4 4 4 4 4 f d f . 
+            . f d f f f f f f f f f f f f . 
+            . f f f . . . . . . . . f f f . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    } else if (newOrientation == "down") {
+        sprites.setDataString(tankCharacter, "orientation", newOrientation)
+        tankCharacter.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . f f f . . . . . . . . f f f . 
+            . f f f f f f f f f f f f d f . 
+            . f d f 4 4 4 4 4 4 4 4 f f f . 
+            . f f f 4 f 4 f f 4 f 4 f d f . 
+            . f d f 4 4 4 4 4 4 4 4 f f f . 
+            . f f f 4 4 2 e e 2 4 4 f d f . 
+            . f d f 4 4 2 e e 2 4 4 f f f . 
+            . f f f 4 4 2 e e 2 4 4 f d f . 
+            . f d f 4 4 4 e e 4 4 4 f f f . 
+            . f f f 4 4 4 e e 4 4 4 f d f . 
+            . f d f f f f e e f f f f f f . 
+            . f f f . . . e e . . . f f f . 
+            . . . . . . . e e . . . . . . . 
+            . . . . . . . e e . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    } else if (newOrientation == "right") {
+        sprites.setDataString(tankCharacter, "orientation", newOrientation)
+        tankCharacter.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . f f f . . . . . . . . f f f . 
+            . f f f f f f f f f f f f d f . 
+            . f d f 4 4 4 4 4 4 4 4 f f f . 
+            . f f f 4 f 4 f f 4 f 4 f d f . 
+            . f d f 4 4 4 4 4 4 4 4 f f f . 
+            . f f f 4 4 2 e e 2 4 4 f d f . 
+            . f d f 4 4 2 e e 2 4 4 f f f . 
+            . f f f 4 4 2 e e 2 4 4 f d f . 
+            . f d f 4 4 4 e e 4 4 4 f f f . 
+            . f f f 4 4 4 e e 4 4 4 f d f . 
+            . f d f f f f e e f f f f f f . 
+            . f f f . . . e e . . . f f f . 
+            . . . . . . . e e . . . . . . . 
+            . . . . . . . e e . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    } else if (newOrientation == "left") {
+        sprites.setDataString(tankCharacter, "orientation", newOrientation)
+        tankCharacter.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . f f f . . . . . . . . f f f . 
+            . f f f f f f f f f f f f d f . 
+            . f d f 4 4 4 4 4 4 4 4 f f f . 
+            . f f f 4 f 4 f f 4 f 4 f d f . 
+            . f d f 4 4 4 4 4 4 4 4 f f f . 
+            . f f f 4 4 2 e e 2 4 4 f d f . 
+            . f d f 4 4 2 e e 2 4 4 f f f . 
+            . f f f 4 4 2 e e 2 4 4 f d f . 
+            . f d f 4 4 4 e e 4 4 4 f f f . 
+            . f f f 4 4 4 e e 4 4 4 f d f . 
+            . f d f f f f e e f f f f f f . 
+            . f f f . . . e e . . . f f f . 
+            . . . . . . . e e . . . . . . . 
+            . . . . . . . e e . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    }
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        f 2 
+        2 f 
+        `, tankCharacter, 0, -50)
+    projectile.y += -5
+})
 function randomWalls () {
     bricksDifficulty = 30
     tiles.setTilemap(tiles.createTilemap(hex`0a0008000000000000000000000004010101010101010104010201020101020102010101010103030101010101010101030301010101010201020101020102010401010101010101010402020202020202020202`, img`
@@ -28,14 +124,23 @@ function randomWalls () {
             e 4 4 4 4 4 4 4 4 4 4 4 4 4 4 e 
             e 4 4 4 4 4 4 4 4 4 4 4 4 4 4 e 
             e e e e e e e e e e e e e e e e 
-            `, SpriteKind.Player)
+            `, SpriteKind.Wall)
         tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
     }
 }
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    setOrientation("down")
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Wall, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    sprite.destroy()
+})
 let mySprite: Sprite = null
 let bricksDifficulty = 0
+let projectile: Sprite = null
+let tankCharacter: Sprite = null
 scene.setBackgroundColor(7)
-let tankCharacter = sprites.create(img`
+tankCharacter = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . e e . . . . . . . 
     . . . . . . . e e . . . . . . . 
@@ -53,6 +158,7 @@ let tankCharacter = sprites.create(img`
     . f f f . . . . . . . . f f f . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+sprites.setDataString(tankCharacter, "orientatation", "up")
 info.setScore(0)
 controller.moveSprite(tankCharacter)
 tankCharacter.setFlag(SpriteFlag.StayInScreen, true)
